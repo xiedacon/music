e24f8c1d4760adff5ce3dd1949fb97698b024b10
xiedacon <xiedacon@qq.com>
2017-03-05 12:24:09 +0800

    重构：歌曲详情页

--- static/js/song.js
+++ static/js/song.js
@@ -1,91 +1,149 @@
 (function() {
-	var songId = PageScope.params.songId;
-	PageScope.loadForFirst = "comment/songId_" + songId;
-	PageScope.loadPageBean = "comment/songId_" + songId + "/";
-	PageScope.page = "song";
-	
-	AJAX({
-		url : "song/" + songId,
-		success : loadSong
-	});
-	AJAX({
-		url : PageScope.loadForFirst,
-		success : FUNCTION.loadForFirst
-	});
+	var id = PageScope.params.id;
 
-	function loadSong(data) {
-		if(data.code != 200){
-			MMR.get("simpleMsg").showError(data.error.value);
-			return;
-		}
-		var song = data.data //
-		, $songEle = $("#song");
+	$.ajax({
+		url : "song/" + id,
+		type : "GET",
+		dataType : "json"
+	}).done(function(source){
+		var template = `
+		<div class="entityMessage_left">
+			<img src="{{song.icon}}">
+		</div>
+		<div class="entityMessage_right">
+			<div class="label">
+				<span class="content">单曲</span>
+				<span class="triangle"><i></i></span>
+			</div>
+			<h2>
+				<span>{{song.name}}</span>
+				{{if song.remark}}
+				<span class="remark">{{song.remark}}</span>
+				{{/if}}
+			</h2>
+			<div class="creator_time_company">
+				<div class="creator">
+					歌手：
+					<a title="{{song.singerName}}" href="#singer?id={{song.singerId}}" class="creatorName">{{song.singerName}}</a>
+				</div>
+				<div class="creator album">
+					所属专辑：
+					<a href="#album?id={{song.albumId}}" title="{{song.albumName}}" class="creatorName">{{song.albumName}}</a>
+				</div>
+			</div>
+			<div class="buttons">
+				<div class="play_addToPlayList">
+					<span class="play">
+						<i class="icomoon"></i>
+						<span>播放</span>
+					</span>
+					<span class="addToPlaylist icomoon"></span>
+				</div>
+				<span class="collection button">
+					<i class="icomoon"></i>收藏
+				</span>
+				<span class="share button">
+					<i class="icomoon"></i>分享
+				</span>
+				<span class="download button">
+					<i class="icomoon"></i>下载
+				</span>
+				<span class="comment button">{{song.commentNum}}</span>
+			</div>
+			<div id="lrc" class="details lrc">
+				<p>暂无歌词</p>
+			</div>
+		</div>
+		`, //
+		data = {
+			song : process(source)
+		};
 
-		$songEle.find(".entityMessage_left img").attr({
-			"src" : song.icon
-		});
-		$songEle.find(".entityMessage_right h2 span").not(".remark").text(song.name);
-		if (song.remark) {
-			$songEle.find(".entityMessage_right h2 .remark").text(song.remark);
-		}
-		$songEle.find(".creator_time_company .creator .creatorName").attr({
-			"title" : song.singerName,
-			"data-href" : "singer?singerId=" + song.singerId
-		}).text(song.singerName);
-		$songEle.find(".creator_time_company .album .creatorName").attr({
-			"title" : song.albumName,
-			"data-href" : "album?albumId=" + song.albumId
-		}).text(song.albumName);
-		$songEle.find(".buttons .comment").html("<i class='icomoon'></i>(" + song.commentNum + ")");
-
-		// 获取歌词
-		AJAX({
-			url : "song/" + PageScope.params.songId + "/lrc",
-			success : function(data) {
-				if (data.code == 200 && data.data) {
-					var $lrcEle = $songEle.find(".details");
-					$lrcEle.children("p").remove();
-					$lrcEle.children(".show_hidden").removeAttr("style").click(function() {
-						if ($(this).attr("data-flag") === "close") {
-							$(this).html("收起<i style='transform: translate(-125%, -25%) rotate(225deg);'></i>").attr("data-flag", "open");
-							$(this).parent().css({
-								"height" : "inherit"
-							})
-						} else {
-							$(this).html("展开<i></i>").attr("data-flag", "close");
-							$(this).parent().css({
-								"height" : "300px"
-							})
-						}
-					});
-					var lrc = data.data;
-					var ar = lrc.match(/(?:\[ar:)(.*)(?:\])/);
-					var ti = lrc.match(/(?:\[ti:)(.*)(?:\])/);
-
-					if (ar) {
-						$lrcEle.append("<p>" + ar[1] + "</p>");
-					}
-					if (ti) {
-						$lrcEle.append("<p>" + ti[1] + "</p>");
-						$lrcEle.append("<p>&nbsp;</p>");
-					}
-
-					var ps = lrc.match(/\[\d{2}:\d{2}\.\d{2}\].*[\n\r]/g);
-					var p;
-					for (var i = 0; i < ps.length; i++) {
-						p = ps[i];
-						var strs = p.match(/(\[\d{2}:\d{2}\.\d{2}\])(.*)[\n\r]/);
-						$lrcEle.append("<p data-time=" + strs[1] + ">" + strs[2] + "</p>");
-					}
-				}
-			}
-		})
-
-		$(".commentList .commentList_detail").text("共" + song.commentNum + "条评论");
+		document.querySelector("div#song").innerHTML = Template.compile(template)(data);
+		document.querySelector("span#commentNum").innerHTML = "共" + data.song.commentNum + "条评论";
 		var user = UserManager.getUser();
 		if (user) {
-			$(".commentList .newComment_left").attr("src", user.icon);
+			document.querySelector("img#userIcon").src = user.icon;
 		}
-		$(".commentList .button").attr("onclick", "MMR.addComment('song','" + song.id + "')");
-	}
+		document.querySelector("span#addComment").addEventListener("click",function(){
+			MMR.addComment('song', data.song.id);
+		});
+
+		$.ajax({
+			url : "song/" + id + "/lrc",
+			type : "GET",
+			dataType : "json"
+		}).done(function(source){
+			var lrc = process(source);
+			if(!lrc){
+				return;
+			}
+
+			var template = `
+			{{if lrc.ar}}
+			<p>{{lrc.ar}}</p>
+			{{/if}}
+			{{if lrc.ti}}
+			<p>{{lrc.ti}}</p>
+			{{/if}}
+			<p>&nbsp;</p>
+			{{each lrc.lyrics as lyric}}
+			<p data-time="{{lyric.time}}">{{lyric.content}}</p>
+			{{/each}}
+			`, //
+			data = {
+				lrc : {
+					get ar (){
+						var ar = lrc.match(/\[ar:(.*)\]/);
+						if(ar){
+							return ar[1];
+						}
+					},
+					get ti (){
+						var ti = lrc.match(/\[ti:(.*)\]/);
+						if(ti){
+							return ti[1];
+						}
+					},
+					get lyrics (){
+						var lyrics = lrc.match(/\[\d{2}:\d{2}\.\d{2}\].*[\n\r]/g);
+						if(lyrics){
+							return lyrics.map(function(lyric){
+								var temp = lyric.match(/^\[(\d{2}:\d{2}\.\d{2})\](.*)[\n\r]$/);
+								return {
+									time : temp[1],
+									content : temp[2]
+								};
+							})
+						}
+					}
+				}
+			};
+
+			document.querySelector("div#lrc").innerHTML = Template.compile(template)(data);
+		})
+	})
+
+	$.ajax({
+		url : "comment/songId_" + id + "/1",
+		type : "GET",
+		dataType : "json"
+	}).done(function(source){
+		Excutor({
+			source : source,
+			data : {
+				urlPrefix : "comment/songId_" + id + "/"
+			},
+			before : function(source, data, excutor){
+				source = process(source);
+				data.hotList = source.hotList;
+				data.pageBean = source.pageBean;
+			},
+			excute : function(source, data, excutor){
+				FUNCTION.loadComments("ul#commentList", data);
+				FUNCTION.loadPages("ul#pages", data, excutor);
+				FUNCTION.loadEmojis();
+			}
+		}).excute();
+	});
 }())
