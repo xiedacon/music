84adb17e969d5bd5f09033e1c9f6c548be01b478
xiedacon <xiedacon@qq.com>
2017-03-08 11:30:55 +0800

    重构：歌单总页

--- static/js/singer-total.js
+++ static/js/singer-total.js
@@ -1,11 +1,13 @@
 (function() {
-	var url = "singer";
+	var url = "singer", //
+	index = Number.MAX_VALUE;
 
 	if(PageScope.params.classifyId !== "all"){
 		url = "singer/classifyId_" + PageScope.params.classifyId;
 	}
 	if(PageScope.params.classifyId === "hot"){
 		url = "singer/hot";
+		index = 10;
 	}
 
 	$.ajax({
@@ -13,180 +15,121 @@
 		type : "GET",
 		dataType : "json"
 	}).done(function(source){
-		
+		var template = `
+		<div class="material_top">
+			<h2>{{h2}}</h2>
+		</div>
+		<ul class="right_buttons">
+			<li class="button double now">热门</li>
+			<li class="button">A</li>
+			<li class="button">B</li>
+			<li class="button">C</li>
+			<li class="button">D</li>
+			<li class="button">E</li>
+			<li class="button">F</li>
+			<li class="button">G</li>
+			<li class="button">H</li>
+			<li class="button">I</li>
+			<li class="button">J</li>
+			<li class="button">K</li>
+			<li class="button">L</li>
+			<li class="button">M</li>
+			<li class="button">N</li>
+			<li class="button">O</li>
+			<li class="button">P</li>
+			<li class="button">Q</li>
+			<li class="button">R</li>
+			<li class="button">S</li>
+			<li class="button">T</li>
+			<li class="button">U</li>
+			<li class="button">V</li>
+			<li class="button">W</li>
+			<li class="button">C</li>
+			<li class="button">Y</li>
+			<li class="button">Z</li>
+			<li class="button double">其他</li>
+		</ul>
+		<ul class="songerList entitys">
+			{{each singers as singer index}}
+			<li class="songer entity" style="{{if (index+1)%5 === 0}}padding:25px 0 0 0{{/if}}">
+				<div class="imageDiv">
+					<a href="#singer?id={{singer.id}}">
+						<img title="{{singer.name}}的音乐" src="{{singer.icon}}">
+					</a>
+				</div>
+				<a title="{{singer.name}}的音乐" class="name" href="#singer?id={{singer.id}}">{{singer.name}}</a>
+				{{if singer.userId}}
+				<a title="{{singer.name}}的个人主页" class="icon" href="#home?id={{singer.userId}}">
+					<i class="icomoon"></i>
+				</a>
+				{{/if}}
+			</li>
+			{{/each}}
+		</ul>
+		{{if remainSinger && remainSinger.length != 0}}
+		<ul class="songerList_simple">
+			{{each remainSinger as singer index}}
+			<li class="singer_simple" style="{{if (index+1)%5 === 0}}padding:0 0 0 0{{/if}}">
+				<a title="{{singer.name}}的音乐" class="name" href="#singer?id={{singer.id}}">{{singer.name}}</a>
+				{{if singer.userId}}
+				<a title="{{singer.name}}的个人主页" class="icon" href="#home?id={{singer.userId}}">
+					<i class="icomoon"></i>
+				</a>
+				{{/if}}
+			</li>
+			{{/each}}
+		</ul>
+		{{/if}}
+		`, //
+		singers = process(source), //
+		data = {
+			h2 : PageScope.params.classifyName,
+			singers : singers.filter(function(singer, i){
+				return i < index;
+			}),
+			remainSinger : singers.filter(function(singer, i){
+				return i >= index;
+			})
+		}
+
+		document.querySelector("div#singers").innerHTML = Template.compile(template)(data);
 	});
-	AJAX({
-		url : url,
-		success : loadFouction
-	});
-	AJAX({
+
+	$.ajax({
 		url : "json/userClassify",
-		success : loadClassify
+		type : "GET",
+		dataType : "json"
+	}).done(function(source){
+		var template = `
+		<ul class="select">
+			<h3>推荐</h3>
+			<a class="option {{if 'hot' === id}}now{{/if}}" href="#singers?classifyId=hot&classifyName=推荐歌手" style="display:block">
+				<i class="point"></i>
+				<span title="推荐歌手" class="songerList">推荐歌手</span>
+			</a>
+			<a class="option {{if 'all' === id}}now{{/if}}" href="#singers?classifyId=all&classifyName=入驻歌手" style="display:block">
+				<i class="point"></i>
+				<span title="入驻歌手" class="songerList">入驻歌手</span>
+			</a>
+		</ul>
+		{{each classifys as classify}}
+		<ul class="select" style="border: 0">
+			<h3>{{classify.name}}</h3>
+			{{each classify.childs as child}}
+			<li class="option {{if child.id === id}}now{{/if}}">
+				<i class="point"></i>
+				<a href="#singers?classifyId={{child.id}}&classifyName={{child.name}}" title="{{child.name}}" class="songerList">{{child.name}}</a>
+			</li>
+			{{/each}}
+		</ul>
+		{{/each}}
+		`, //
+		data = {
+			id : PageScope.params.classifyId,
+			classifys : source
+		};
+
+		document.querySelector("div#classifys").innerHTML = Template.compile(template)(data);
 	});
 
-	function loadClassify(classifyList) {
-
-		if (!classifyList) {
-			return;
-		}
-
-		var $classifyListEle = $("#classifyList");
-		var $prototype = $classifyListEle.children(".prototype").clone().removeClass("prototype");
-		var $classifyEle, classify;
-
-		var childId = PageScope.params.classifyId;
-
-		for (var i = 0; i < classifyList.length; i++) {
-			$classifyEle = $prototype.clone();
-			classify = classifyList[i];
-
-			$classifyEle.find("h3").text(classify.name);
-
-			var childs = classify.childs;
-			var $_prototype = $classifyEle.children(".prototype").clone().removeClass("prototype");
-			var $child, child;
-			for (var j = 0; j < childs.length; j++) {
-				$child = $_prototype.clone();
-				child = childs[j];
-
-				$child.find(".songerList").attr({
-					"data-href" : "singers?classifyId=" + child.id + "&classifyName=" + child.name,
-					"title" : child.name
-				}).text(child.name);
-
-				if (childId == child.id) {
-					$child.addClass("now");
-				}
-
-				$classifyEle.append($child);
-			}
-
-			$classifyListEle.append($classifyEle);
-		}
-	}
-
-	function loadFouction(data) {
-		if(data.code != 200){
-			MMR.get("simpleMsg").showError(data.error.value);
-			return;
-		}
-
-		var data = data.data;
-		$("#singerList").css("display", "none");
-		$("#simpleSingerList").css("display", "none");
-		switch (PageScope.params.classifyId) {
-			case "all" :
-				$("h2").text("入驻歌手");
-				$("#classify_all").addClass("now");
-				loadAll(data);
-				break;
-			case "hot" :
-				$("h2").text("热门歌手");
-				$("#classify_hot").addClass("now");
-				loadHot(data);
-				break;
-			default :
-				$("h2").text(PageScope.params.classifyName);
-				loadByClassify(data);
-				break;
-		}
-	}
-
-	function loadByClassify(singerList) {
-		loadSingerList(singerList);
-
-		// ajax下拉加载
-
-	}
-
-	function loadHot(data) {
-		var top10List = new Array();
-		var remainList = new Array();
-		for (var i = 0; i < data.length; i++) {
-			if (i < 10) {
-				top10List[i] = data[i];
-			} else {
-				remainList[i - 10] = data[i];
-			}
-		}
-
-		loadSingerList(top10List);
-		loadSimpleSingerList(remainList);
-	}
-
-	function loadAll(singerList) {
-		loadSingerList(singerList);
-
-		// ajax下拉加载
-
-	}
-
-	function loadSingerList(singerList) {
-		var $singerListEle = $("#singerList");
-
-		$singerListEle.removeAttr("style") //
-		.children().not(".prototype").remove();
-
-		var $prototype = $singerListEle.find(".prototype").clone().removeClass("prototype");
-		var $singerEle, singer;
-
-		for (var i = 0; i < singerList.length; i++) {
-			$singerEle = $prototype.clone();
-			singer = singerList[i];
-
-			$singerEle.find("img").attr({
-				"title" : singer.name + "的音乐",
-				"src" : singer.icon,
-				"data-href" : "singer?singerId=" + singer.id
-			});
-			$singerEle.find(".name").attr({
-				"title" : singer.name + "的音乐",
-				"data-href" : "singer?singerId=" + singer.id
-			}).text(singer.name);
-			if (singer.userId) {
-				$singerEle.find(".icon").removeAttr("style").attr({
-					"title" : singer.name + "的个人主页",
-					"data-href" : "homePage?userId=" + singer.userId
-				});
-			}
-
-			if ((i + 1) % 5 === 0) {
-				$singerEle.css("padding", "25px 0 0 0");
-			}
-
-			$singerListEle.append($singerEle);
-		}
-	}
-
-	function loadSimpleSingerList(singerList) {
-		var $simpleSingerListEle = $("#simpleSingerList");
-
-		$simpleSingerListEle.removeAttr("style") //
-		.children().not(".prototype").remove();
-
-		var $prototype = $simpleSingerListEle.find(".prototype").clone().removeClass("prototype");
-		var $simpleSingerEle, singer;
-
-		for (var i = 0; i < singerList.length; i++) {
-			$simpleSingerEle = $prototype.clone();
-			singer = singerList[i];
-
-			$simpleSingerEle.find(".name").attr({
-				"title" : singer.name + "的音乐",
-				"data-href" : "singer?singerId=" + singer.id
-			}).text(limitStringLength(singer.name,5.5));
-			if (singer.userId) {
-				$simpleSingerEle.find(".icon").removeAttr("style").attr({
-					"title" : singer.name + "的个人主页",
-					"data-href" : "homePage?userId=" + singer.userId
-				});
-			}
-
-			if ((i + 1) % 5 === 0) {
-				$simpleSingerEle.css("padding", "0");
-			}
-
-			$simpleSingerListEle.append($simpleSingerEle);
-		}
-	}
 }())
